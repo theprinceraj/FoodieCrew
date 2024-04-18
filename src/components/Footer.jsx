78915eda57447c5aafbda25e547327de8e217bcc
theprinceraj <profile.princeraj@gmail.com>
2024-04-19 03:02:13 +0530

    Update Footer component with new contact information and remove Pricing link
    The Footer component has been updated with new contact information, including a new address, email, phone number, and fax number. The "Pricing" link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -71,7 +71,7 @@ export default function Footer() {
                 <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
                 <p>
                   <a href="#!" className="text-reset">
-                    Pricing
+                    Jump to Top
                   </a>
                 </p>
                 <p>
@@ -94,17 +94,18 @@ export default function Footer() {
               <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                 <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
                 <p>
-                  <i className="fas fa-home me-3"></i> New York, NY 10012, US
+                  <i className="fas fa-home me-3"></i> 37, Kolkata, India -
+                  700056
                 </p>
                 <p>
                   <i className="fas fa-envelope me-3"></i>
                   profile.princeraj@gmail.com
                 </p>
                 <p>
-                  <i className="fas fa-phone me-3"></i> + 01 234 567 88
+                  <i className="fas fa-phone me-3"></i> + 91 919 932 2677
                 </p>
                 <p>
-                  <i className="fas fa-print me-3"></i> + 01 234 567 89
+                  <i className="fas fa-print me-3"></i> + 91 919 933 5622
                 </p>
               </div>
             </div>
