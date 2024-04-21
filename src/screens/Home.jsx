b8bd25d38d4ab7c82dab0788c7aa1c89bd2b5b8a
theprinceraj <profile.princeraj@gmail.com>
2024-04-21 15:29:54 +0530

    .

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,11 +1,16 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import FoodCard from "../components/foodCard";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      Home
+      <FoodCard
+        itemName={"Cheese"}
+        itemDescription={"Cheese is good"}
+        redirectURL={"#"}
+      />
       <Footer />
     </>
   );
