e0a089d13d83bcc86600597bd6846bc9c60fab73
theprinceraj <profile.princeraj@gmail.com>
2024-04-25 02:47:37 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
 // import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import Signup from "./screens/Signup.jsx";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
   },
 ]);
 
