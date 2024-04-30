c870b0e1355cff1a02f9014fbe92adfb0d994903
theprinceraj <profile.princeraj@gmail.com>
2024-04-30 23:23:29 +0530

    ,

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,11 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
-import cors from "cors";
-app.use(cors());
 
 import express from "express";
 const app = express();
 const PORT = 3000;
+
+import cors from "cors";
+app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
