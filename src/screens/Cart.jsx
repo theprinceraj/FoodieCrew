b6860aef8a670c19c0402e81b128e2676ed2bfe1
theprinceraj <profile.princeraj@gmail.com>
2024-05-05 10:53:31 +0530

    "Added modal for empty cart alert and updated checkout logic"
    This commit updates the Cart.jsx file by adding a modal to display an alert when the cart is empty during checkout. It also adjusts the checkout logic to

--- src/screens/Cart.jsx
+++ src/screens/Cart.jsx
@@ -3,28 +3,36 @@ import { useCart } from "../components/ContextReducer";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { useDispatchCart } from "../components/ContextReducer";
+import Modal from "../Modal";
+import { useState } from "react";
 export default function Cart() {
+  const [alertEmptyModalView, setAlertEmptyModalView] = useState(false);
+
   const data = useCart();
   const dispatch = useDispatchCart();
   const handleCheckout = async () => {
-    let userEmail = localStorage.getItem("userEmail");
-    let response = await fetch("http://localhost:3000/api/updateOrderData", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: userEmail,
-        order_data: data,
-        order_date: new Date().toUTCString(),
-      }),
-    });
+    if (data.length > 0) {
+      let userEmail = localStorage.getItem("userEmail");
+      let response = await fetch("http://localhost:3000/api/updateOrderData", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: userEmail,
+          order_data: data,
+          order_date: new Date().toUTCString(),
+        }),
+      });
 
-    if (response.ok) {
-      dispatch({ type: "DROP" });
-      let json = await response.json();
-      if (json.success) {
+      if (response.ok) {
+        dispatch({ type: "DROP" });
+        let json = await response.json();
+        if (json.success) {
+        }
       }
+    } else {
+      setAlertEmptyModalView(true);
     }
   };
 
@@ -35,6 +43,13 @@ export default function Cart() {
       <div className="container" onClick={handleCheckout}>
         <span className="btn bg-success text-white">Checkout</span>
       </div>
+      {alertEmptyModalView ? (
+        <Modal onClose={() => setAlertEmptyModalView(false)}>
+          <div className="container">
+            Please add something in the cart before checking out.
+          </div>
+        </Modal>
+      ) : null}
       <div className="position-absolute bottom-0 w-100">
         <Footer />
       </div>
