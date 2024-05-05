45a0571875010b38cb4fa46b32ac07ffff48c9c3
theprinceraj <profile.princeraj@gmail.com>
2024-05-05 11:26:58 +0530

    Change API endpoints to remove "/api" suffix
    * BACKEND\_PRODUCTION\_ENDPOINT changed from "<https://foodie-crew-backend.vercel.app/api>" to "<https://foodie-

--- endpoints.js
+++ endpoints.js
@@ -1,4 +1,4 @@
 export const ENDPOINTS = {
-  BACKEND_PRODUCTION_ENDPOINT: "https://foodie-crew-backend.vercel.app/api",
-  BACKEND_DEVELOPMENT_ENDPOINT: "http://localhost:3000/api",
+  BACKEND_PRODUCTION_ENDPOINT: "https://foodie-crew-backend.vercel.app",
+  BACKEND_DEVELOPMENT_ENDPOINT: "http://localhost:3000",
 };
